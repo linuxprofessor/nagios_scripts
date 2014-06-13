b019c1ee13a85ea16c69ece9df7ffc6e1e9eed50
jlweatherly <josh@josh-weatherly.com>
2014-06-13 11:33:43 -0500

    add -Avg argument
    Adding -Avg argument which can be used to take the average temperature of all CPU cores instead of one specific one.

--- check_cputemp_freebsd/check_cputemp_freebsd.py
+++ check_cputemp_freebsd/check_cputemp_freebsd.py
@@ -17,11 +17,12 @@ coretempstat = int(os.popen("kldstat|grep coretemp|wc -l").read())
 
 # Check if above binary really exists
 if coretempstat == 0:
-	print "coretemp kernel module not loaded. Exiting..."
-	raise SystemExit(3)
+        print "coretemp kernel module not loaded. Exiting..."
+        raise SystemExit(3)
 
 # Parse arguments
 parser = argparse.ArgumentParser(description='Check CPU temperature on FreeBSD using coretemp')
+parser.add_argument('-Avg', action="store_true", dest="avg", default=False, help='Get average temperature of all CPU cores')
 parser.add_argument('-C', action="store", dest="core", type=int, help='Core, starting with 0')
 parser.add_argument('-w', action="store", dest="warn", type=int, help='Warning temperature')
 parser.add_argument('-c', action="store", dest="crit", type=int, help='Critical temperature')
@@ -31,28 +32,54 @@ results = parser.parse_args()
 core = results.core
 warn = results.warn
 crit = results.crit
-if (warn == None or crit == None or core == None):
-	parser.print_help()
-	raise SystemExit()
+avg = results.avg
+if (warn == None or crit == None):
+        parser.print_help()
+        raise SystemExit()
+if (core == None and avg == False):
+        parser.print_help()
+        raise SystemExit()
 if (crit <= warn):
-	print "Critical temperature can't be less or equal to warning temperature"
-	parser.print_help()
-	raise SystemExit()
+        print "Critical temperature can't be less or equal to warning temperature"
+        parser.print_help()
+        raise SystemExit()
 
 # Read the CPU temperature
 try:
-	sensor = int(os.popen("sysctl -a | grep temperature | grep '\." + str(core) + "\.' | awk '{print $2}'|awk -F. '{print $1}'").read())
+        if (avg == True):
+                sensors = str(os.popen("sysctl -a | grep temperature | grep '\.cpu\.' | awk '{print $2}'|awk -F. '{print $1}'").read())
+                sensors = sensors.strip().split()
+                average = 0.0
+                my_sum = 0.0
+                temps = "" # holds string of core temps for output
+                for n in sensors:
+                        my_sum = my_sum + int(n)
+                        temps = temps + n + ","
+                average =  my_sum / len(sensors)
+                temps = temps + "Avg " + "{0:.2f}".format(average)
+                #print "Core Temps:", temps
+                #print "Total:", my_sum, " Len:", len(sensors), " Average:", average
+                sensor = average # store our final result into the original variable sensor so comparison code below can be used unmodified
+        else:
+                sensor = int(os.popen("sysctl -a | grep temperature | grep '\." + str(core) + "\.' | awk '{print $2}'|awk -F. '{print $1}'").read())
 except:
-	print "Error reading CPU temperature"
-	raise SystemExit(3)
+        print "Error reading CPU temperature"
+        raise SystemExit(3)
 
 # Print status
-if (sensor < warn):
-	print "OK: Temperature: " + str(sensor) + " C|Temperature=" + str(sensor) + ";" + str(warn) + ";" + str(crit) + ";" + str(warn-5) + ";" + str(crit+5)
-	raise SystemExit(0)
-elif (sensor >= warn and sensor < crit):
-	print "WARNING: Temperature: " + str(sensor) + " C|Temperature=" + str(sensor) + ";" + str(warn) + ";" + str(crit) + ";" + str(warn-5) + ";" + str(crit+5)
-	raise SystemExit(1)
+if (avg == True):
+        output = "Avg Temp: " + "{0:.2f}".format(sensor) + " C|Temps=" + str(temps) + ";" + str(warn) + ";" + str(crit) + ";" + str(warn-5) + ";" + str(crit+5)
 else:
-	print "CRITICAL: Temperature: " + str(sensor) + " C|Temperature=" + str(sensor) + ";" + str(warn) + ";" + str(crit) + ";" + str(warn-5) + ";" + str(crit+5)
-	raise SystemExit(2)
+        output = "Temperature: " + str(sensor) + " C|Temperature=" + str(sensor) + ";" + str(warn) + ";" + str(crit) + ";" + str(warn-5) + ";" + str(crit+5)
+
+if (sensor < warn):
+ print "OK:", output
+ raise SystemExit(0)
+elif (sensor >= warn and sensor < crit):
+ print "WARNING:", output
+ raise SystemExit(1)
+else:
+ print "CRITICAL:", output
+ raise SystemExit(2)
+
+
